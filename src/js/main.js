18f88dca7db286c9dc180d0dd2084337e6df82cc
smirnova <olha.smirnova@nixsolutions.com>
2024-02-23 11:24:27 +0200

    fixed shadow and hover on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
 import BaseHelpers from './helpers/base-helpers';
 import BurgerMenu from './modules/burger-menu';
+import MobileChecker from "./helpers/mobile-checker.js";
 
 BaseHelpers.checkWebpSupport();
 
 BaseHelpers.addTouchClass();
+
+const mobileChecker = MobileChecker.isAny;
+
+if (!mobileChecker) {
+    document.body.classList.add("desktop");
+} else (
+    document.body.classList.remove("desktop")
+)
 
 new BurgerMenu().init();
